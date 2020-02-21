7e725cf1884466ea11f8b94eb732ca548443c3da
Thomas Burkhalter <burkhalter@puzzle.ch>
2020-02-21 17:09:53 +0100

    Add StatusController

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'status/health', to: 'status#health'
+  get 'status/readiness', to: 'status#readiness'
 end
+
