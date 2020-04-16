56247aef0da6cd6e665e04aa873f8c32599cf4d3
Carlo Beltrame <carlo@beltra.me>
2020-04-16 11:12:47 +0200

    Change header and button font to Styrene

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,8 @@ module Quartieridee
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Precompile fonts
+    config.assets.paths << Rails.root.join("app","assets","fonts")
   end
 end
