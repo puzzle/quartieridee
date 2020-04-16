c0333a7d9796dadfc0bbec3c8ae6b9a53a670f06
Carlo Beltrame <carlo@beltra.me>
2020-04-16 13:38:52 +0200

    Fix bundler / rubygems / rails bug

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -6,4 +6,8 @@ bin/rails db:migrate RAILS_ENV=development
 
 echo "Decidim::System::Admin.where(email: 'quartieridee@puzzle.ch').first_or_initialize.update!(password: 'quartieridee', password_confirmation: 'quartieridee')" | bundle exec rails console
 
+# Fix bundler / rubygems / rails bug that causes puma to fail
+# https://github.com/rubygems/rubygems/issues/3279
+bundle binstubs bundler --force
+
 exec "$@"
