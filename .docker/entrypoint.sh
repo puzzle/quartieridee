c04469e8aadc6cc2f572530af00094b16539a279
Carlo Beltrame <carlo@beltra.me>
2020-04-22 12:03:01 +0200

    Print out invitation link on startup, to work around broken email config

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -6,6 +6,14 @@ bin/rails db:migrate RAILS_ENV=development
 
 echo "Decidim::System::Admin.where(email: 'quartieridee@puzzle.ch').first_or_initialize.update!(password: 'quartieridee', password_confirmation: 'quartieridee')" | bundle exec rails console
 
+if [ ! -f "/db-init/done" ]; then
+  echo 'ActiveRecord::Base.connection.execute(IO.read(".docker/database-init.sql"))' | bundle exec rails console
+  echo 'user = Decidim::User.where(email: "quartieridee@puzzle.ch").first; user.invite!; "*************************************************** ACTIVATE YOUR ACCOUNT **************************************************** #{user.email} on http://localhost:3000/users/invitation/accept?invitation_token=#{user.raw_invitation_token}"' | bundle exec rails console
+  date > /db-init/done
+else
+  echo "Skipping database initialization because already done on $(cat /db-init/done)"
+fi
+
 # Fix bundler / rubygems / rails bug that causes puma to fail
 # https://github.com/rubygems/rubygems/issues/3279
 bundle binstubs bundler --force
