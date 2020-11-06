35032efc2f07c83413dc050683272c3f0101b808
Thomas Burkhalter <burkhalter@puzzle.ch>
2020-11-06 13:33:52 +0100

    Revert "Set default ordering of proposals to most recent"
    This reverts commit 7efc5b41e53116e82215a32cae43d7ae5b70459c.

--- config/initializers/quartieridee_customization.rb
+++ config/initializers/quartieridee_customization.rb
@@ -1,5 +1,4 @@
 Decidim::DiffCell.include Quartieridee::DiffCell
 Decidim::Proposals::ProposalMCell.include Quartieridee::Proposals::ProposalMCell
-Decidim::Proposals::ProposalsController.include Quartieridee::Proposals::ProposalsController
 Decidim::ParticipatoryProcesses::ProcessMCell.include Quartieridee::ParticipatoryProcesses::ProcessMCell
 Decidim::Debates::CreateDebateEvent.include Quartieridee::Debates::CreateDebateEvent
